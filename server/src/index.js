d2afc17f41f15f4bec5c293823d21769e1a0a638
Matheus Blödorn <blodorn.matheus@gmail.com>
2017-10-17 00:02:33 -0200

    append client bundle to server-side rendered html

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,26 @@
 import express from 'express'
 import React from 'react'
-import {renderToString} from 'react-dom/server'
+import { renderToString } from 'react-dom/server'
 import Home from './client/components/home'
 const app = express()
 
-app.get('/', (req, res)=>{
-const content = renderToString(<Home />)
-res.send(content)
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />)
+  const html =
+  `
+  <html>
+    <head></head>
+    <body>
+      <div>${content}</div>
+      <script src="bundle.js"></script>
+    </body>
+  </html>
+  `
+  res.send(html)
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
